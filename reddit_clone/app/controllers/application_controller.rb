bd12093cf6644ea5f894648913486f7efd548e0a
sean <midruss@yahoo.com>
2022-01-21 14:03:30 -0500

    Finished application controller methods and user controller methods

--- reddit_clone/app/controllers/application_controller.rb
+++ reddit_clone/app/controllers/application_controller.rb
@@ -1,14 +1,17 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+    
     def current_user
         return nil if session[:session_token].nil?
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
     def ensure_logged_in
-
+        redirect_to new_session_url unless logged_in?
     end
 
     def login!(user)
+        @current_user = user
         session[:session_token] = user.reset_session_token!
     end
 
--- reddit_clone/app/controllers/users_controller.rb
+++ reddit_clone/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+    before_action :ensure_logged_in, only: [:show, :index]
+
     def new
         @user = User.new 
     end
@@ -14,6 +16,16 @@ class UsersController < ApplicationController
             flash.now[:errors] = @user.errors.full_messages
             render :new
         end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end
+
+    def index
+        @users = User.all 
+        render :index 
     end
 
     def user_params
