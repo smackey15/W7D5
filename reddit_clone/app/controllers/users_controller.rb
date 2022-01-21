21a90d12e0666fc3a1e752e37f9a68e10bd2ffed
hwkcode <hwkcode@gmail.com>
2022-01-21 12:16:27 -0500

    Add application_controller methods

--- reddit_clone/app/controllers/users_controller.rb
+++ reddit_clone/app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
         @user = User.new 
     end
 
-    
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            redirect_to subs_url
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
